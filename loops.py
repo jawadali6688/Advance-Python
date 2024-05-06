6611b9792f440d7c5649bdfc01cbb3f5f2bcdbc2
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-05-06 23:36:53 +0500

    some problems

---
+++ loops.py
@@ -0,0 +1,35 @@
+# z = 5
+
+# for x in range(1,11):
+#     print(x*z)
+
+# Problem One
+# numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+# x = 0
+# positve_numbers = 0
+# while x < len(numbers) :
+#     if numbers[x] > 0:
+#         positve_numbers = positve_numbers + 1
+#     x = x + 1
+# print(positve_numbers)   
+
+# Problem Two 
+
+# n = 10
+
+# even_count = 0
+
+# for i in range(1, n):
+#     if i % 2 == 0:
+#         even_count = i + even_count
+
+# print(even_count)
+
+# Problem 03 
+
+number = 5
+
+for i in range(1,11):
+    if i == 5:
+        continue
+    print(f'{i} X {number} = {i*number}')
