bed3e9158a22d98c663405dd4152cbf5c7be3904
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-05-06 10:21:18 +0500

    some other topics

--- immutable.py
+++ immutable.py
@@ -34,3 +34,88 @@
 # }
 # myDic["username"] = "Khan"
 # print(myDic["username"])
+
+# import math
+# import random
+# print(math.pi)
+# print(random.random())
+
+# myTuple = ("C", "M", "D")
+# print(random.choice(myTuple))
+
+# username = "Jawad Khan"
+# username[8] = "me" 
+# print(username[0:5])
+# print(username[-8])
+# print(dir(username))
+# print(len(username))
+
+# myList = ["Jawad Khan", "It's me", True, False, ["This is another"]]
+
+# print((myList[4][0]))
+
+
+# myDict = {
+#     "Name": "Jawad",
+#     "Roll": "S22RPHYS1M01025",
+#     "Department": "Physics and AI"
+# }
+
+# print(myDict["Name"], myDict["Department"], myDict["Roll"])
+
+# myTuple = ("Jawad Khan", "This is me")
+# print(myTuple[1])
+
+# myList = None
+# myDict = {}
+# print(myList, myDict)
+
+
+# import sys
+
+# print(sys.getrefcount("Jawad"))
+# print(sys.getrefcount(2345343))
+# print(sys.getrefcount(1))
+
+
+# myListOne = [1,2,3]
+
+# myListTwo = myListOne
+
+# myListOne = [43,3]
+# print(myListOne)
+# print(myListTwo)
+
+# n = [1,2,3]
+# m = n
+# m = [45,5]
+# print(n)
+# print(m)
+
+# z = [1,2,3]
+# x = [4,5,4]
+# z[0] = 34
+# print(z)
+# print(x)
+
+# i = [1,2,3]
+# j = i[:]
+# i[0] = 44
+# print(i)
+# print(j)
+
+
+# import copy
+
+# j = [1,3,5, [45,34, [45]]]
+# i = copy.copy(j)
+# print(i)
+
+a = [1,43]
+b = a
+
+c = [1,43]
+d = [1,43]
+print( a is b)
+print(c is d)
+
